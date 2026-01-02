025ed92390e9252e4925e068dd5e07126479dd01
nvmmonsalud <nvm.monsalud@gmail.com>
2026-01-02 17:59:18 +0800

    Add testing, linting, and CI tooling

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es2022: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  ignorePatterns: ['node_modules/', 'dist/'],
+};
